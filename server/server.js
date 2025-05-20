e16a91f34abdd28b6506078ffe7fb5082544715e
ThienTM27 <ubw1212@gmail.com>
2025-05-20 14:38:30 +0700

    Install mssql in server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mssql = require('mssql');
+
+const connection = mssql.createConnection()
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
