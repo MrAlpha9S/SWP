67cddf4ee0e9590bff1152ab102254ca3b4c39be
MrAlpha9S <mr28042005@gmail.com>
2025-07-28 16:33:45 +0700

    [Update Coach]

--- server/routes/topicRoute.js
+++ server/routes/topicRoute.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const topicRouter = express.Router();
 const { handleGetTopic } = require('../controllers/topicController');
-const { handleGetBlog, handlePostBlog, handleGetPostsOfUser} = require('../controllers/blogController');
+const{ handleGetBlogById } = require('../controllers/adminController');
+const { handleGetBlog, handlePostBlog, handleGetPostsOfUser, handleGetPostedBlog} = require('../controllers/blogController');
 const checkJwt = require('../middlewares/jwtChecker');
 
 //profileRouter.get('/getProfile', jwtCheck, handleGetProfile);
 topicRouter.get('/get-blogs/:userAuth0Id', checkJwt, handleGetPostsOfUser)
-topicRouter.post('/post_blog', handlePostBlog);
+topicRouter.post('/getblog', checkJwt, handleGetPostedBlog);
+topicRouter.get('/getblogbyid/:id', checkJwt, handleGetBlogById);
+topicRouter.post('/post_blog', checkJwt, handlePostBlog);
 topicRouter.get('/:topic_id', checkJwt, handleGetTopic);
 topicRouter.get('/:topic_id/:blog_id', checkJwt, handleGetBlog);
 
