13d6abdf894082bd32bc4e99d254fa832af8fed4
ThienTM27 <ubw1212@gmail.com>
2025-07-29 21:54:22 +0700

    Minor adjustments

--- client/src/components/utils/getBackendURL.js
+++ client/src/components/utils/getBackendURL.js
@@ -8,7 +8,7 @@ export function getBackendUrl() {
 
 export function getFrontEndUrl() {
     if (import.meta.env.MODE === 'production') {
-        return import.meta.env.VITE_DEV_BACKEND_URL;
+        return import.meta.env.VITE_PRODUCTION_BACKEND_URL;
     } else {
         return import.meta.env.VITE_PRONTEND_DEV_URL;
     }
